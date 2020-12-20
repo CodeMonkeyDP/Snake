f48c1ea919d60b2ac549d6e372df9b090d0e843e
ComeMonkeyDP <diman.prok98@gmail.com>
2020-12-20 21:58:28 +0300

    Перенос на Java

---
+++ Server/src/com/company/GUI.java
@@ -0,0 +1,61 @@
+package com.company;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+
+public class GUI {
+    //Создадим окно и установим заголовок
+    JFrame window = new JFrame("Сервер");
+    //Создадим панель
+    JPanel panel = new JPanel();
+    //Создадим интерфейс
+    JLabel lbIp = new JLabel("IP-адрес:");
+    JTextField textFieldIp = new JTextField("127.0.0.1");
+    JLabel lbPort = new JLabel("Порт:");
+    JTextField textFieldPort = new JTextField("8888");
+    JButton buttonStart = new JButton("Запустить сервер");
+
+    ServerThread serv;
+
+
+    public GUI() {
+
+        //Событие "закрыть" при нажатии по крестику окна
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Событие для запуска сервера
+        buttonStart.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                serv = new ServerThread(textFieldIp.getText(), textFieldPort.getText());
+                serv.run();
+            }
+        });
+
+        GridLayout grid = new GridLayout(5, 0, 5, 12);
+        panel.setLayout(grid);
+
+
+
+        //Добавим интерфейс на панель
+        panel.add(lbIp);
+        panel.add(textFieldIp);
+        panel.add(lbPort);
+        panel.add(textFieldPort);
+        panel.add(buttonStart);
+
+
+        //Добавим панель в окно
+        window.getContentPane().add(panel);
+
+        window.pack();
+
+        //Разместим программу по центру
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
---
+++ Server/src/com/company/ServerThread.java
@@ -0,0 +1,75 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ServerThread implements Runnable {
+    ExecutorService executeIt = Executors.newFixedThreadPool(2);
+    // Сервер
+    ServerSocket server;
+    // IP
+    String ip;
+    // Порт
+    String port;
+
+    public ServerThread(String ip, String port){
+        this.ip = ip;
+        this.port = port;
+    }
+
+
+    @Override
+    public void run() {
+        // стартуем сервер на порту и инициализируем переменную для обработки консольных команд с самого сервера
+        try (ServerSocket server = new ServerSocket(Integer.valueOf(port));
+
+             BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+            System.out.println("Server socket created, command console reader for listen to server commands");
+
+            // стартуем цикл при условии что серверный сокет не закрыт
+            while (!server.isClosed()) {
+
+                // проверяем поступившие комманды из консоли сервера если такие
+                // были
+                if (br.ready()) {
+                    System.out.println("Main Server found any messages in channel, let's look at them.");
+
+                    // Получаем команду
+                    String serverCommand = br.readLine();
+
+                    // если команда - quit то инициализируем закрытие сервера и
+                    // выход из цикла раздачии нитей монопоточных серверов
+                    if (serverCommand.equalsIgnoreCase("quit")) {
+                        System.out.println("Main Server initiate exiting...");
+                        server.close();
+                        break;
+                    }
+                }
+
+                // если комманд от сервера нет то становимся в ожидание
+                // подключения к сокету общения под именем - "clientDialog" на
+                // серверной стороне
+                Socket client = server.accept();
+
+                // после получения запроса на подключение сервер создаёт сокет
+                // для общения с клиентом и отправляет его в отдельную нить
+                // в Runnable(при необходимости можно создать Callable)
+                // монопоточную нить = сервер - MonoThreadClientHandler и тот
+                // продолжает общение от лица сервера
+                executeIt.execute(new MonoThreadClientHandler(client));
+                System.out.print("Connection accepted.");
+            }
+
+            // закрытие пула нитей после завершения работы всех нитей
+            executeIt.shutdown();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
